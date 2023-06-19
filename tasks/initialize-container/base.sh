d3885cbc225af32b8a6aa58bfa49b9e7011dd9f8
pan93412 <pan93412@gmail.com>
2023-06-20 01:26:13 +0800

    Initiate the first version

---
+++ tasks/initialize-container/base.sh
@@ -0,0 +1,7 @@
+#!/usr/bin/env bash
+# -*- coding: utf-8 -*-
+
+sudo apt update
+sudo apt install nginx -y
+sudo snap install zellij nvim --classic -y
+sudo apt upgrade -y
---
+++ tasks/initialize-container/init_fish.sh
@@ -0,0 +1,6 @@
+#!/usr/bin/env bash
+
+sudo apt install fish
+
+curl https://raw.githubusercontent.com/oh-my-fish/oh-my-fish/master/bin/install | fish
+chsh -s /usr/bin/fish "$USER"
